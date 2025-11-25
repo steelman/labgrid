steelman/labgrid

commit d9c24f04d1c5ecb9e0a85e51a1f762cd81277e6f

hidrelay: add resource, driver and export
Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

labgrid/remote/exporter.py
@@ -432,6 +432,25 @@ class USBDeditecRelaisExport(USBGenericExport):
             'index': self.local.index,
         }
 
+@attr.s(eq=False)
+class USBHIDRelayExport(USBGenericExport):
+    """ResourceExport for outputs on simple USB HID relays"""
+
+    def __attrs_post_init__(self):
+        super().__attrs_post_init__()
+
+    def _get_params(self):
+        """Helper function to return parameters"""
+        return {
+            'host': self.host,
+            'busnum': self.local.busnum,
+            'devnum': self.local.devnum,
+            'path': self.local.path,
+            'vendor_id': self.local.vendor_id,
+            'model_id': self.local.model_id,
+            'index': self.local.index,
+        }
+
 
 exports["AndroidFastboot"] = USBGenericExport
 exports["IMXUSBLoader"] = USBGenericExport
@@ -449,6 +468,7 @@ exports["USBTMC"] = USBGenericExport
 exports["SiSPMPowerPort"] = SiSPMPowerPortExport
 exports["USBPowerPort"] = USBPowerPortExport
 exports["DeditecRelais8"] = USBDeditecRelaisExport
+exports["HIDRelay"] = USBHIDRelayExport
 
 
 @attr.s

labgrid/driver/__init__.py
@@ -30,3 +30,4 @@ from .xenadriver import XenaDriver
 from .dockerdriver import DockerDriver
 from .lxaiobusdriver import LXAIOBusPIODriver
 from .pyvisadriver import PyVISADriver
+from .usbhidrelay import HIDRelayDriver

labgrid/driver/usbhidrelay.py
@@ -1,0 +1,49 @@
+# pylint: disable=no-member
+import attr
+
+from .common import Driver
+from ..factory import target_factory
+from ..resource.remote import NetworkHIDRelay
+from ..step import step
+from ..protocol import DigitalOutputProtocol
+from ..util.agentwrapper import AgentWrapper
+
+
+@target_factory.reg_driver
+@attr.s(eq=False)
+class HIDRelayDriver(Driver, DigitalOutputProtocol):
+    bindings = {
+        "relay": {"HIDRelay", NetworkHIDRelay},
+    }
+
+    def __attrs_post_init__(self):
+        super().__attrs_post_init__()
+        self.wrapper = None
+
+    def on_activate(self):
+        if isinstance(self.relay, NetworkHIDRelay):
+            host = self.relay.host
+        else:
+            host = None
+        self.wrapper = AgentWrapper(host)
+        self.proxy = self.wrapper.load('usb_hid_relay')
+
+    def on_deactivate(self):
+        self.wrapper.close()
+        self.wrapper = None
+        self.proxy = None
+
+    @Driver.check_active
+    @step(args=['status'])
+    def set(self, status):
+        if self.relay.invert:
+            status = not status
+        self.proxy.set(self.relay.busnum, self.relay.devnum, self.relay.index, status)
+
+    @Driver.check_active
+    @step(result=True)
+    def get(self):
+        status = self.proxy.get(self.relay.busnum, self.relay.devnum, self.relay.index)
+        if self.relay.invert:
+            status = not status
+        return status

labgrid/resource/remote.py
@@ -270,6 +270,17 @@ class NetworkDeditecRelais8(RemoteUSBResource):
 
 @target_factory.reg_resource
 @attr.s(eq=False)
+class NetworkHIDRelay(RemoteUSBResource):
+    """The NetworkHIDRelay describes a remotely accessible USB relay port"""
+    index = attr.ib(default=1, validator=attr.validators.instance_of(int))
+    invert = attr.ib(default=False, validator=attr.validators.instance_of(bool))
+    def __attrs_post_init__(self):
+        self.timeout = 10.0
+        super().__attrs_post_init__()
+
+
+@target_factory.reg_resource
+@attr.s(eq=False)
 class NetworkSysfsGPIO(NetworkResource, ManagedResource):
     manager_cls = RemotePlaceManager
 

labgrid/resource/udev.py
@@ -580,3 +580,15 @@ class DeditecRelais8(USBResource):
         # Deditec does not set proper model/vendor IDs, so we use ID_MODEL instead
         self.match['ID_MODEL'] = 'DEDITEC_USB-OPT_REL-8'
         super().__attrs_post_init__()
+
+
+@target_factory.reg_resource
+@attr.s(eq=False)
+class HIDRelay(USBResource):
+    index = attr.ib(default=1, validator=attr.validators.instance_of(int))
+    invert = attr.ib(default=False, validator=attr.validators.instance_of(bool))
+
+    def __attrs_post_init__(self):
+        self.match['ID_VENDOR_ID'] = '16c0'
+        self.match['ID_MODEL_ID'] = '05df'
+        super().__attrs_post_init__()

labgrid/util/agents/usb_hid_relay.py
@@ -1,0 +1,68 @@
+"""
+This module implements the communication protocol to switch the digital outputs
+on a "dcttech" USB Relay.
+
+Supported modules:
+
+- USBRelay2 (and likely others)
+
+Supported Functionality:
+
+- Turn digital output on and off
+"""
+import usb.core
+import usb.util
+
+GET_REPORT = 0x1
+SET_REPORT = 0x9
+REPORT_TYPE_FEATURE = 3
+
+
+class USBHIDRelay:
+    def __init__(self, **args):
+        self._dev = usb.core.find(**args)
+        if self._dev is None:
+            raise ValueError("Device not found")
+        if self._dev.is_kernel_driver_active(0):
+            self._dev.detach_kernel_driver(0)
+
+    def set_output(self, number, status):
+        assert 1 <= number <= 8
+        req = [0xFF if status else 0xFD, number]
+        resp = self._dev.ctrl_transfer(
+            usb.util.CTRL_TYPE_CLASS | usb.util.CTRL_RECIPIENT_DEVICE | usb.util.ENDPOINT_OUT,
+            SET_REPORT,
+            (REPORT_TYPE_FEATURE << 8) | 0, # no report ID
+            0,
+            req, # payload
+        )
+
+    def get_output(self, number):
+        assert 1 <= number <= 8
+        resp = self._dev.ctrl_transfer(
+            usb.util.CTRL_TYPE_CLASS | usb.util.CTRL_RECIPIENT_DEVICE | usb.util.ENDPOINT_IN,
+            GET_REPORT,
+            (REPORT_TYPE_FEATURE << 8) | 0, # no report ID
+            0,
+            8, # size
+        )
+        return bool(resp[7] & (1 << (number-1)))
+
+    def __del__(self):
+        usb.util.release_interface(self._dev, 0)
+
+
+def handle_set(busnum, devnum, number, status):
+    relay = USBHIDRelay(bus=busnum, address=devnum)
+    relay.set_output(number, status)
+
+
+def handle_get(busnum, devnum, number):
+    relay = USBHIDRelay(bus=busnum, address=devnum)
+    return relay.get_output(number)
+
+
+methods = {
+    'set': handle_set,
+    'get': handle_get,
+}

tests/test_agent.py
@@ -106,7 +106,10 @@ def test_all_modules():
     methods = aw.list()
     assert 'sysfsgpio.set' in methods
     assert 'sysfsgpio.get' in methods
-
+    aw.load('usb_hid_relay')
+    methods = aw.list()
+    assert 'usb_hid_relay.set' in methods
+    assert 'usb_hid_relay.get' in methods
 
 def test_import_modules():
     import labgrid.util.agents

commit c801afe6b2f3ab2f3fe86eea1734f7cd2fa50a81

hidrelay: export the invert parameter
Export the invert parameter from USBHIDRelay to make it available
to labgrid-client and other remote clients.

Follow the pattern of USBFlashableExport and call super()._get_params()
to avoid code duplication.

Fixes: d9c24f04 ("hidrelay: add resource, driver and export")
Signed-off-by: Łukasz Stelmach <l.stelmach@samsung.com>

labgrid/remote/exporter.py
@@ -515,16 +515,10 @@ class USBHIDRelayExport(USBGenericExport):
 
     def _get_params(self):
         """Helper function to return parameters"""
-        return {
-            "host": self.host,
-            "busnum": self.local.busnum,
-            "devnum": self.local.devnum,
-            "path": self.local.path,
-            "vendor_id": self.local.vendor_id,
-            "model_id": self.local.model_id,
-            "index": self.local.index,
-        }
-
+        p = super()._get_params()
+        p["index"] = self.local.index
+        p["invert"] = self.local.invert
+        return p
 
 @attr.s(eq=False)
 class USBFlashableExport(USBGenericExport):